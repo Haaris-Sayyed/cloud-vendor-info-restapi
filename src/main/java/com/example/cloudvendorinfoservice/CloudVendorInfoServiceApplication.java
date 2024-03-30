6858e86337d8de197eaf3b124139c10dd8d8ec7b
Haaris Sayyed <haaris7200@gmail.com>
2024-03-30 16:15:31 +0530

    Initial commit

---
+++ src/main/java/com/example/cloudvendorinfoservice/CloudVendorInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudvendorinfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudVendorInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudVendorInfoServiceApplication.class, args);
+	}
+
+}
